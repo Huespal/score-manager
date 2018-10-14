e1386835432d8ef8694fcf749bcbfa82c69d61d9
Sanlu <dani.sanlu@blackdata.systems>
2018-10-14 14:49:06 +0200

    Initial commit

---
+++ src/modules/scoreRange/score.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+//import './ScoreRange.css';
+
+class ScoreRange extends Component {
+  render() {
+    return (
+      <input type="range" />
+    );
+  }
+}
+
+export default ScoreRange;
